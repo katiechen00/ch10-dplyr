c062ca914f6bc1eba665317f3c74708045526911
katiechen00 <chenzi7@uw.edu>
2018-04-20 21:41:04 -0700

    update

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,43 +6,50 @@
 #install.packages("devtools")
 #devtools::install_github("hadley/fueleconomy")
 library(fueleconomy)
+devtools::install_github("hadley/fueleconomy")
 
 # Install and load the "dplyr" library
-
+library(dplyr)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+manufacturers <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+distinct(manufacturers)
 
 # Filter the data set for vehicles manufactured in 1997
-
+vehicle_1997 <- filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
-
+vehicle_1997 <- arrange(vecicle_1997, hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+vehicle_1997 <- mutate(vehicle_1997, average = (hwy + cty) / 2)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+vehicle_two_wheel <- filter(vehicles, drive == "2-Wheel Drive") %>% 
+  filter(cty > 20)
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
-
+worst_hwy_id <- filter(vehicle_two_wheel, hwy == min(hwy)) %>%
+  select(id)
+  
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+most_hwy <- function(year_choice, make_choice) {
+  filter(vehicles, make == make_choice, year == year_choice) %>% 
+  filter(hwy == max(hwy)) %>% 
+  select(model)
+}
 
 # What was the most efficient Honda model of 1995?
-
+most_hwy(1995, "Honda")
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -11,14 +11,21 @@ library(fueleconomy)
 
 # Which 2015 Acura model has the best hwy MGH? (Use dplyr, but without method
 # chaining or pipes--use temporary variables!)
-
+model_year <- filter(vehicles, year == 2015, make == "Acura")
+best_hwy <- filter(model_year, hwy == max(hwy))
+best_model <- select(best_hwy, model)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr, nesting functions)
-
+best_acura <- select(
+  filter(
+    filter(vehicles, year == 2015, make == "Acura"), hwy == max(hwy)
+  ), model
+)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr and the pipe operator)
-
-
+best_acura_2015 <- filter(vehicles, year == 2015, make == "Acura") %>% 
+  filter(hwy == max(hwy)) %>% 
+  select(model)
 
 ### Bonus
 
